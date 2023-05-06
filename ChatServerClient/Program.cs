778c4f8363ba2af6d44e53181dd410c5e3f52ea2
Karol Gutkowski <01171298@pw.edu.pl>
2023-05-06 18:17:10 +0200

    testing server chatting functionality

--- ChatServerClient/Program.cs
+++ ChatServerClient/Program.cs
@@ -11,25 +11,32 @@ namespace ChatServerClient // Note: actual namespace depends on the project name
         static IPEndPoint server = new(new IPAddress(new byte[] { 127, 0, 0, 1 }), 10_000);
         static void Main(string[] args)
         {
-            client.ConnectAsync(server);
+            try
+            {
+                client.Connect(server);
+            }catch(SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            Console.WriteLine("Connected to server!");
             NetworkStream stream = client.GetStream();
+            
 
             string? toSend = string.Empty;
             while (toSend != "quit()")
             {
-                var buffer = new byte[1_024];
-            int received = stream.Read(buffer);
 
-            var message = Encoding.UTF8.GetString(buffer, 0, received);
-            Console.WriteLine($"Message received: \"{message}\"");
-
-
-
-            
                 toSend = Console.ReadLine();
                 if (toSend is null) break;
                 var bytesToSend = Encoding.UTF8.GetBytes(toSend);
                 stream.Write(bytesToSend);
+
+                var buffer = new byte[1_024];
+                int received = stream.Read(buffer);
+
+                var message = Encoding.UTF8.GetString(buffer, 0, received);
+                Console.WriteLine($"Message received: \"{message}\"");
             }
 
             stream.Close();
